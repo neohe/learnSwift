7d9cdfd19f763fa88022e295308abb9604739014
neohe <neo.he@hotmail.com>
2015-07-06 00:35:10 +0800

    switch sentence

--- learn.swift
+++ learn.swift
@@ -33,3 +33,15 @@ var greeting = "Hello!"
 if let name = optName {
 	greeting = "Hello, \(name)"
 }
+
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+	let vegetableComment = "Add some raisins and make ants on a log."
+case "cucumber", "watercress":
+	let vegetableComment = "That would make a good tea sandwich."
+case let x where x.hasSuffix("pepper"):
+	let vegetableComment = "Is it a spicy \(x)?"
+default:
+	let vegetableComment = "Everything tastes good in soup."
+}
