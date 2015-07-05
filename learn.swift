ee513fcacf259894a559263337b46b128837ef90
neohe <neo.he@hotmail.com>
2015-07-05 23:46:03 +0800

    qiangzhi leixingzhuanhuan

--- learn.swift
+++ learn.swift
@@ -7,3 +7,7 @@ let myConstant = 40
 let implicitInteger = 10
 let implicitDouble = 10.0
 let explicitDouble: Double = 10
+
+let label = "The widthh is"
+let width = 94
+let widthLabel = label + String(width)
