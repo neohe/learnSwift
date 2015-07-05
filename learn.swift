f42e9a21f5afb887b43b6c18c37474fdb434fd5e
neohe <neo.he@hotmail.com>
2015-07-05 23:47:43 +0800

    leixingzhuanhuan easier

--- learn.swift
+++ learn.swift
@@ -11,3 +11,8 @@ let explicitDouble: Double = 10
 let label = "The widthh is"
 let width = 94
 let widthLabel = label + String(width)
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
