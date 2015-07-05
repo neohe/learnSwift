791fe24d7992dd115f4691e69c44770e06be63c8
neohe <neo.he@hotmail.com>
2015-07-06 00:41:56 +0800

    for in

--- learn.swift
+++ learn.swift
@@ -45,3 +45,18 @@ case let x where x.hasSuffix("pepper"):
 default:
 	let vegetableComment = "Everything tastes good in soup."
 }
+
+let interestingNumbers = [
+	"Prime": [2, 3, 5, 7, 11, 13],
+	"Fibonacci": [1, 1, 2, 3, 5, 8],
+	"Square": [1, 4, 9, 16, 25],
+]
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+	for num in numbers {
+		if num > largest {
+			largest = num
+		}
+	}
+}
+largest
