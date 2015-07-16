93bdda97e610a76bf366d60ce49a31847ca45b32
neohe <neo.he@hotmail.com>
2015-07-17 00:04:14 +0800

    how to use dict

--- dictionary.swift
+++ dictionary.swift
@@ -2,3 +2,28 @@
 var airports: Dictionary<String, String> = ["TYO": "Tokyo", "DUB": "Dublin"]
 // same as above
 var airports = ["TYO": "Tokyo", "DUB": "Dublin"]
+
+// 只读属性count
+println("The dictionary of airports contains \(airports.count) items.")
+
+// add
+airports["LHD"] = "London"
+
+// change
+airports["LHD"] = "London Heathrow"
+
+// updateValue(forKey:)
+if let oldValue = airports.updateValue("Dublin International", forKey: "DUB")
+{
+	println("The old value for DUB was \(oldValue).")
+}
+
+// 可选类型
+if let airportName = airports["DUB"]
+{
+	println("The name of the airport is \(airportName).")
+}
+else
+{
+	println("That airport is not in the dictionary airports.")
+}
