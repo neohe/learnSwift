56c1f78a4540ac8e70e5666081a8aa28c311342a
neohe <neo.he@hotmail.com>
2015-08-08 18:31:54 +0800

    控制转移语句之break

--- kongzhizhuanyi.swift
+++ kongzhizhuanyi.swift
@@ -14,3 +14,30 @@ for chr in puzzleInput
     }
 }
 println(puzzleOutput)
+
+
+# break
+let numberSymbol: Character = "f"
+possibleValue: Int?
+switch numberSymbol
+{
+case "1", "o":
+    possibleValue = 1
+case "2", "t":
+    possibleValue = 2
+case "3", "th":
+    possibleValue = 3
+case "4", "f":
+    possibleValue = 4
+default:
+    break
+}
+
+if let iValue = possibleValue
+{
+    println("The integer value of \(numberSymbol) is \(iValue)")
+}
+else
+{
+    println("The integer value could not be found.")
+}
